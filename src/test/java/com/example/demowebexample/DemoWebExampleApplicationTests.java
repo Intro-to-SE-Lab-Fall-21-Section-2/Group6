274156ffb7ba57aea7612143fffe8cbaaf3a295e
darrell cleveland <dcleveland@camgian.com>
2021-10-01 23:05:36 -0500

    SE-2: create backend demo for ECS

---
+++ src/test/java/com/example/demowebexample/DemoWebExampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demowebexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebExampleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
